cb44f25c5b4bb0ec3270580848075236d7120880
karinesant3 <135091496+karinesant3@users.noreply.github.com>
2023-06-14 22:47:04 -0300

    Add files via upload

---
+++ TRABAI - Copia/app/metodoMap.js
@@ -0,0 +1,7 @@
+function aplicarDesconto(livros) {
+    const desconto = 0.3
+    livrosComDesconto = livros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)} //copia de toda array para outro objeto
+    })
+    return livrosComDesconto
+}
